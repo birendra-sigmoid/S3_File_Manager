050b42b2f37c0ece2e38aa9cda1bda6621f7fa85
birendra-sigmoid <91470475+birendra-sigmoid@users.noreply.github.com>
2021-10-13 15:44:04 +0530

    Add files via upload

---
+++ s3details.py
@@ -0,0 +1,24 @@
+import boto3
+from config import S3_BUCKET, S3_KEY, S3_SECRET
+from pprint import pprint
+
+def _get_s3_resource():
+    if S3_KEY and S3_SECRET:
+        return boto3.resource(
+            's3',
+            aws_access_key_id=S3_KEY,
+            aws_secret_access_key=S3_SECRET
+        )
+    else:
+        return boto3.resource('s3')
+
+
+def get_buckets_list():
+    client = boto3.client('s3')
+
+    return client.list_buckets().get('Buckets')
+
+
+def get_bucket():
+    s3_resource = _get_s3_resource()
+    return s3_resource.Bucket(S3_BUCKET)
